8aba88bca4047c49ca717eddf692ffc94e19f61e
Matheus Kautzmann <mkvchain@gmail.com>
2021-03-12 11:24:16 -0300

    Add explicit config for babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,14 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: false
+                }
+              ]
+            ]
           }
         }
       }
